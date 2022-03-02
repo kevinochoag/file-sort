0f53cde16a570d40eb77523e6b42b11b6434bca7
Kevstrosky <kevin8ag@gmail.com>
2022-03-02 11:36:28 -0700

    First version, it can move files  by type files between folders

---
+++ File Sort/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace File_Sort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnSelectSource = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "jpg",
+            "pdf"});
+            this.comboBox1.Location = new System.Drawing.Point(184, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Source folder";
+            // 
+            // selectSourceFolder
+            // 
+            this.BtnSelectSource.Location = new System.Drawing.Point(182, 53);
+            this.BtnSelectSource.Name = "selectSourceFolder";
+            this.BtnSelectSource.Size = new System.Drawing.Size(146, 23);
+            this.BtnSelectSource.TabIndex = 3;
+            this.BtnSelectSource.Text = "Select folder";
+            this.BtnSelectSource.UseVisualStyleBackColor = true;
+            this.BtnSelectSource.Click += new System.EventHandler(this.selectSourceFolder);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Path..";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Target folder";
+            // 
+            // selectTargetFolder
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 82);
+            this.button2.Name = "selectTargetFolder";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Select folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.selectTargetFolder);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(334, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Path..";
+            // 
+            // SortFileButton
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 141);
+            this.button3.Name = "sortFileButton";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Ready";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.sortFileButton);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(416, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 348);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnSelectSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "File Sort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnSelectSource;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ File Sort/Form1.cs
@@ -0,0 +1,70 @@
+﻿/* 
+ * File Sort
+ * Start of development on february 23rd, 2022
+ * Made by Kevstrosky
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Sort
+{
+    public partial class Form1 : Form
+    {
+        //I created these two FolderBrowserDialog, one for the source folder that we are gonna grab the files what we want and the other one for put all of them 
+        FolderBrowserDialog sourceFolder = new FolderBrowserDialog();
+        FolderBrowserDialog targetFolder = new FolderBrowserDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectSourceFolder(object sender, EventArgs e) //This method show us the folder browser dialog to select the source folder
+        {
+                if (sourceFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(sourceFolder.SelectedPath))
+                {
+                    label3.Text = sourceFolder.SelectedPath;
+                }
+            
+         }
+
+        private void selectTargetFolder(object sender, EventArgs e) //This method show us the folder browser dialog to select the target folder
+        {
+            
+                if (targetFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(targetFolder.SelectedPath))
+                {
+                    label5.Text = targetFolder.SelectedPath;
+                }
+                
+        }
+
+        private void sortFileButton(object sender, EventArgs e) //This method sends the files selected by the data type in the combo box from the source folder to the target folder
+        {
+            String command = "move " + sourceFolder.SelectedPath+@"\"+"*"+"."+ comboBox1.Text +" "+ targetFolder.SelectedPath + @"\";
+            ExecuteCommand(command);
+            MessageBox.Show("All files have been transferred!");
+
+        }
+        static void ExecuteCommand(String command) //This method execute the command line that we send to it
+        {
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = false; //This line of code does not allow the cmd window to open
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            Console.WriteLine(result);
+
+        }
+    }
+}
